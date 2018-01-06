bb5631555607e101251b746126a7e33f7397a4ce
quan pl <34340053+quanpi@users.noreply.github.com>
2018-01-06 18:55:05 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -98,8 +98,6 @@ angular.module('myApp', [
 	mySocket.on('connect', function() {
 		console.log("connected")
 		mySocket.emit("RAIN") //Cập nhập trạng thái mưa
-		mySocket.emit("LED_STATUS")
-		mySocket.emit("LED_UPDATE")
 		$scope.updateServo(0) //Servo quay về góc 0 độ!. Dùng cách 2 
 	})
 		
